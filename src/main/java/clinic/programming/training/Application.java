b9eb6206dcc9515b9c09a0d7de27a9c4b0da40b8
alexgarciafranco <agfranco@minsait.com>
2020-10-27 12:08:09 +0100

    Adding Application Java source

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,12 @@
+public class Application{
+
+	public Application(){
+		Sytem.out.println("Inside Application");
+	}
+
+	public static void main(String[]args){
+		System.out.println("Starting Application");
+		Application app = new Application();
+	}
+
+}
